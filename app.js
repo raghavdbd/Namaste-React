25e398c2bc32f9447d37d241cadc46aa19bc334c
raghav goyal <raghavgoyaldbd4782gmail.com>
2023-11-01 08:42:18 +0530

    Ch3.Laying the Foundation

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOm from "react-dom/client"
+const HeadinfComponent =()=>{
+    return <h1>dfdfdf       </h1>
+
+}
+const jsxheading=<h1>Namaste React</h1>
+
+const root=ReactDOm.createRoot(document.getElementById("root"));
+
+root.render(jsxheading);
